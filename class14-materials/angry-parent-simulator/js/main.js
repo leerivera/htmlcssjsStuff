6554df0cb7cdeb619aec697b17bf6cbd70c521b6
leerivera <leerivera80@gmail.com>
2022-03-03 14:24:33 -0500

    test3

--- class14-materials/angry-parent-simulator/js/main.js
+++ class14-materials/angry-parent-simulator/js/main.js
@@ -2,5 +2,11 @@ document.querySelector("#yell").addEventListener('click', run);
 
 function run(){
    firstName = document.querySelector("#firstName").value;
-   alert(firstName)
+   fMiddleName = document.querySelector("#firstMiddle").value;
+   fLastMiddle = document.querySelector("#lastMiddle").value;
+   lastName = document.querySelector("#lastName").value;
+
+   document.querySelector("#placeToYell").innerText = `${firstName} ${fMiddleName} ${fLastMiddle} ${lastName}`
+
 }
+ 
