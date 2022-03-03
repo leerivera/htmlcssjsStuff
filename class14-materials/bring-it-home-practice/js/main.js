d112808ff858e0eb640cec4a1de18427a6aef7d7
leerivera <leerivera80@gmail.com>
2022-03-03 14:51:31 -0500

    test4

--- class14-materials/bring-it-home-practice/js/main.js
+++ class14-materials/bring-it-home-practice/js/main.js
@@ -1,11 +1,24 @@
 // *Variables*
 // Create a variable and console log the value
+let thing = "quelque chose"
 
+console.log(thing);
 // Create a variable, add 10 to it, and alert the value
+let num = 20;
+alert(num + 10);
 
 // *Functions*
 // Create a function that subtracts 4 numbers and alerts the difference
+document.querySelector("#pressIt").addEventListener("click", run);
 
+function run(){
+    let numOne = 7;
+    let numTwo = 190;
+    let numThree = 34009;
+    let numFour = 15;
+
+    alert(numOne + numFour + numThree + numTwo);
+}
 // Create a function that divides one number by another and returns the remainder
 
 // *Conditionals*
