3019929910021d9b1ef3c2bee1bd78fbbfe04e93
leerivera <leerivera80@gmail.com>
2022-03-06 15:33:10 -0500

    another4

--- class14-materials/bring it again/js/main.js
+++ class14-materials/bring it again/js/main.js
@@ -8,3 +8,8 @@ function multiplysThreeNumbers(n1, n2, n3) {
     const product = n1 * n2 * n3
     alert(product)
 }
+
+function fourNumsAddTwoSubTwo(n1, n2, n3, n4) {
+    const result = n1 + n2 - n3 - n4
+    console.log(result)
+}
